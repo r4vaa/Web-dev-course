fcd7fd7d62c2041798173c3f43751a13af31a3c8
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-06 12:13:25 +0530

    if-else Conditional Statement

--- week1-4/javaScript/day1/app.js
+++ week1-4/javaScript/day1/app.js
@@ -1,3 +1,50 @@
-console.log("Hello from our first javascript file");
+//IF-ELSE Conditional Statement
 
-let total= 1+3;
+// console.log("BEFORE CONDITIONAL")
+// if(1 + 2 === 2){
+//   console.log("Math still works!")
+// }
+
+// console.log("AFTER CONDITIONAL")
+
+// let random = Math.random();
+
+// if( random < 0.5){
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!");
+//   console.log(random);
+// }
+
+// if(random > 0.5){
+//   console.log("YOUR NUMBER IS GREATER THAN 0.5!!");
+//   console.log(random);
+// }
+
+// let num = 19;
+
+// if(num % 2 === 0){
+//   console.log("even")
+// }
+
+// const dayOfWeek = prompt("enter a day:").toLowerCase();
+
+// if(dayOfWeek === 'friday'){
+//   console.log("its funny");
+// }else if(dayOfWeek === 'tuesday'){
+//   console.log("its no funny");
+// }else if(dayOfWeek ===  'monday'){
+//   console.log("its fucking hell day");
+// }
+// else{
+//   console.log("MEH");
+// }
+// const age = 555;
+
+// if(age < 5){
+//   console.log("you are a baby, you get in for free");
+// }else if (age < 10){
+//   console.log("you are a teen you, you have to pay $10");
+// }else if(age < 65){
+//   console.log("You are an adult, you have to pay $20");
+// }else{
+//   console.log("you are a senior, you pay $10");
+// }
