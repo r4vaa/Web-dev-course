8acb297d3b9d15f684225e992003081142d3cac6
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-07 18:03:57 +0530

    Array Methods -  push and pop

---
+++ week1-4/javaScript/arrays/app.js
@@ -0,0 +1,22 @@
+// BASIC ARRAY'S
+
+// const days  = ['Monday','Tuesday','Wedensday','Thursday'];
+// console.log(days[1]);
+// console.log(days.length);
+
+//ARRAY METHODS
+//PUSH ,POP,SHIFT,UNSHIFT - COMMONLY USED 
+//CONCAT, INCLUDES, INDEXOF,JOIN ,REVERSE, SLICE, SPLICE,SORT
+
+//PUSH AND POP.
+//let barbell = [];
+// barbell.push(45) 
+//barbell.push(10)
+//barbell.push(25)
+//barbell.push(50)
+//barbell.push(60)
+// this above code will add these weights to the array
+
+
+//barebell.pop();
+//this code will remove the last value of that array for us 60 will be removed from that array
