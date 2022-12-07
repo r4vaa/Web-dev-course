4957c57ac3ebb14fb248526aa286fe682011db1b
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-07 15:24:15 +0530

    Switch Case Statement

--- week1-4/javaScript/day1/app.js
+++ week1-4/javaScript/day1/app.js
@@ -144,3 +144,39 @@
 //   console.log("You are not an adult anymore");
 // }
 
+//SWITCH CASE STATEMENT
+
+// const day = 1;
+
+// switch(day){
+//   case 1:
+//     console.log("MONDAY");
+//     break;
+
+//     case 2:
+//       console.log("TUESDAY");
+//       break;
+
+//     case 3:
+//       console.log("WEDNESDAY");
+//       break;
+
+//     case 4:
+//       console.log("THURSDAY");
+//       break;
+
+//     case 5:
+//         console.log("FRIDAY");
+//         break;
+    
+//     case 6:
+//         console.log("SATURDAY ");
+//         break;
+//     case 7:
+//       console.log("SUNDAY");
+//       break;
+
+//     default:
+//       console.log("NOT A DAY");
+//       break;
+// }
