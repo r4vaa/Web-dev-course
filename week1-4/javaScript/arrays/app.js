3ccdd84b47255394c4c03b608c252e9303cf7a10
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-07 19:47:59 +0530

    arrays-const, nested arrays

--- week1-4/javaScript/arrays/app.js
+++ week1-4/javaScript/arrays/app.js
@@ -66,3 +66,20 @@
 //LET NUMS =[1,2,3] ;
 //LET NUMSCOPY = NUMS; SO THE VARIABLE NUMSCOPY IS LINKED TO THE NUMS SO IT IS REFERENCED WITH THE SAME MEMOMRY LOCATION
 // SO WHATEVER WE DO WITH NUMS OR NUMSCOPY WILL BE REFLECTED TO BOTH THE ARRAYS AND WHEN DOING EQUALITY IT WILL GIVE US THE VALUE OF TRUE.
+
+
+//Nested Arrays Exercise
+// with an array called airplaneSeats, which represents the rows of seats on a flight.  Our friend Hugo is looking to book a seat on our flight. Fortunately, there is one middle seat available (it's set to null currently).  Please update the array element that is currently null, to instead be "Hugo".
+
+// DO NOT TOUCH!!! (please)
+// const airplaneSeats = [
+//   ['Ruth', 'Anthony', 'Stevie'],
+//   ['Amelia', 'Pedro', 'Maya'],
+//   ['Xavier', 'Ananya', 'Luis'],
+//   ['Luke', null, 'Deniz'],
+//   ['Rin', 'Sakura', 'Francisco']
+// ];
+
+// airplaneSeats[3][1]='Hugo';
+
+// so basically we can update any value inside the given array.
