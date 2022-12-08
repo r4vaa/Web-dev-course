7154d6167ed6172ebe11f6366f60f08d316f878d
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-08 15:58:08 +0530

    creating,accessing,modifying,valid keys in objects

--- week1-4/javaScript/objects/app.js
+++ week1-4/javaScript/objects/app.js
@@ -1,1 +1,50 @@
-console.log("hello");
+//  OBJECTS
+//objects are data structures
+//objects are collection of properties
+//properties are a key-value pair
+//Rather than accessing data using an index, we use custom keys
+
+//syntax
+// const fitBit = {
+//   totalSteps : 999999,
+//   totalMiles : 10808,
+//   avgCalBurn : 9999,
+//   workoutThisWeek : 22,
+   
+// }
+
+//Property = key : value 
+//key-value pairs
+//creating an objects: we can store all types of data
+
+
+// to access information
+// syntax: objectName.keyName
+// objectName['keyName'];
+//valid keys-  all keys are converted into strings, except for symbols
+
+// we can call a variable which is declared having the same value and variable value, but for calling a variable we use
+//syntax- objectName.variableName
+//syntax - objectName[variableName]
+
+//Accessing data in objects, updating and modifying is really simple
+//syntax: const midterms = { dheeraj : 79 , piyu:88}
+//syntax: midterms.dheeraj = 78 || midterms["dheeraj"]=99
+
+// to create a new object key-value
+//syntax:  midterms.manish = 99 || midterms["manish"]=90
+
+//array + object
+
+// const comments = [
+//   {
+//     username : "dheeraj",
+//     text  : "loll",
+//     votes : 99
+//   },
+//   {
+//     username : "piyu",
+//     text  : "adasda0",
+//     votes : 78
+//   }
+// ]
