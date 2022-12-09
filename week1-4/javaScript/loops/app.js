3723ea39f104e9d0c72f3fcd0e3604ccd7220c96
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-09 15:38:31 +0530

    basic loop syntax

---
+++ week1-4/javaScript/loops/app.js
@@ -0,0 +1,10 @@
+// LOOPS
+//LOOPS Allows us to repeat code
+//print "hello" 10 times
+//sum all numbers in an array
+// there are multiple types
+
+//syntax
+// for(let i = 1; i <= 10; i++){
+//   console.log(i);
+// }
