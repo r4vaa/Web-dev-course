271acaf5063d1301c604aed28b5f10f541154f2f
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-09 15:56:35 +0530

    basic understanding of loops(incrementation)

--- week1-4/javaScript/loops/app.js
+++ week1-4/javaScript/loops/app.js
@@ -8,3 +8,27 @@
 // for(let i = 1; i <= 10; i++){
 //   console.log(i);
 // }
+
+//Loop Practice
+// Use a for loop to print out the string "Da ba dee da ba daa" exactly 6 times.  No more, and no less.
+//for(let i=1; i <= 6; i++){
+//   console.log(i + " " +"Da ba dee da ba daa");
+// }
+
+//print even number
+// for(let i = 0; i <= 20; i += 2){
+//   console.log(i);
+// }
+// //print odd numbers
+// for(let i = 1; i<=20; i += 2){
+//   console.log(i);
+// }
+//subtraction
+// for(let i = 100; i >= 0; i-=10){
+//   console.log(i);
+// }
+
+//Multiplication
+// for(let i = 10; i <=100000000000; i *=10){
+//   console.log(i);
+// }
