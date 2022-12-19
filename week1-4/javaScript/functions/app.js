5d71d8268eadee985d67a88ab2ea3502826e6dfa
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-19 10:48:39 +0530

    funcitons

---
+++ week1-4/javaScript/functions/app.js
@@ -0,0 +1,13 @@
+// function printHeart(){
+//   console.log("<3");
+// }
+
+// printHeart();
+function sing() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+sing();
+
+
