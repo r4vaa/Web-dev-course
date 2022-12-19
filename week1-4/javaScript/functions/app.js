4883ac8d32fb65b36f911ccedf3c203324f29360
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-19 16:30:00 +0530

    practice of multiple arguments

--- week1-4/javaScript/functions/app.js
+++ week1-4/javaScript/functions/app.js
@@ -63,3 +63,22 @@
 
 
 //CONCATENATION STR + STR = STR STR || STR1 += STR2 =  STR1STR2
+// Multiple Args Exercise
+// In some dice games like Craps, a roll of two 1's is called "Snake Eyes".  It's generally not a good roll.  Please write a function called isSnakeEyes, which accepts two numbers as inputs, representing two dice.  If the two numbers are both 1's, please print "Snake Eyes!" otherwise print "Not Snake Eyes!"
+
+// isSnakeEyes(1,1) //Snake Eyes!
+// isSnakeEyes(1,5) //Not Snake Eyes!
+// isSnakeEyes(4,5) //Not Snake Eyes!'
+
+
+// function isSnakeEyes(num1,num2){
+//       if(num1 === 1 && num2 === 1){
+//         console.log("Snake eyes");
+//       }else{
+//         console.log("not snake eyes");
+//       }
+// }
+
+// isSnakeEyes(2,2);
+// isSnakeEyes(1,5);
+// isSnakeEyes(4,5);
