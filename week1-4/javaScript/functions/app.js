5632069ef3e7deede79026ce1d3fdc84a4384470
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2022-12-19 11:13:30 +0530

    functions with arguments

--- week1-4/javaScript/functions/app.js
+++ week1-4/javaScript/functions/app.js
@@ -3,11 +3,40 @@
 // }
 
 // printHeart();
-function sing() {
-    console.log("DO");
-    console.log("RE");
-    console.log("MI");
-}
-sing();
+// function sing() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// }
+// sing();
 
+// function greet(person){
+//   console.log(`hi ${person}`);
+// }
+// greet("manish");
+// greet("dheeraj");
 
+// function rollDice(person){
+//   let dice = Math.floor(Math.random()*6)+1;
+//   console.log(`${person} got this: ${dice}`);
+// }
+ 
+// rollDice("dheeraj");
+// rollDice("Manish");
+// rollDice("Meet");
+
+// function rant(message){
+//   console.log(message.toUpperCase());
+// }
+
+// rant("I hate beets");
+// rant("I hate beets");
+// rant("I hate beets");
+
+// function rant(message){
+//   console.log(message.toUpperCase());
+//   console.log(message.toUpperCase());
+//   console.log(message.toUpperCase());
+// }
+
+// rant("I hate beets");
