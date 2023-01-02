2e51e95687fefdbe277bbd93d6f74585ef8934db
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-02 23:03:43 +0530

    ForEach

--- week1-4/javaScript/methods/app.js
+++ week1-4/javaScript/methods/app.js
@@ -7,3 +7,42 @@
 //Filter
 //Some & Every
 //REDUCE
+
+// for each
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+
+
+// numbers.forEach(function (el){
+//    if(el % 2 === 0 ){
+//     console.log(el)
+//   }
+// })
+
+
+// const movies = [
+//   {
+//     title : 'one piece',
+//     score : 90
+//   },
+//   {
+//     title : 'naruto',
+//     score : 99
+//   },
+//   {
+//     title : 'parasite',
+//     score : 95
+//   },
+//   {
+//     title : 'Alien',
+//     score : 93
+//   },{
+//     title : 'Predator',
+//     score : 91
+//   }
+// ];
+
+// movies.forEach(function(movie){
+//   console.log(`${movie.title} ${movie.score}/100`);
+// })
