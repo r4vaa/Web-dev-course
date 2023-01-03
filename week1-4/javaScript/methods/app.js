c0a250127022fd46a88354a3902da6976275603d
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-03 10:45:44 +0530

    Map Method

--- week1-4/javaScript/methods/app.js
+++ week1-4/javaScript/methods/app.js
@@ -46,3 +46,61 @@
 // movies.forEach(function(movie){
 //   console.log(`${movie.title} ${movie.score}/100`);
 // })
+
+// THE MAP Method
+
+// const texts = ['rofl','lol','omg','ttyl']
+// const caps = texts.map(function(t){
+//    return t.toUpperCase() ;
+// })
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+// const double = numbers.map(function(num){
+//   return num * 2;
+// })
+
+
+
+// const movies = [
+//     {
+//       title : 'one piece',
+//       score : 90
+//     },
+//     {
+//       title : 'naruto',
+//       score : 99
+//     },
+//     {
+//       title : 'parasite',
+//       score : 95
+//     },
+//     {
+//       title : 'Alien',
+//       score : 93
+//     },{
+//       title : 'Predator',
+//       score : 91
+//     }
+//   ];
+
+//   const titles = movies.map(function(movie){
+//     return movie.title.toUpperCase();
+//   }) 
+// Map Practice
+// It's time to get practice with the map method!
+
+// Define a variable named firstNames and assign it to the result of mapping over the existing array, fullNames, so that firstNames contains only the first names of the Harry Potter characters from the fullNames array.
+
+
+// Please note:
+
+// The fullNames array is an array of objects with each object containing properties for the first and last names of each character. You may need to click the "Reset code" link if you do not see the fullNames array pre-loaded into the exercise's index.js file.
+
+// This exercise has been updated, you may see Q&A threads from this lecture that relate to the original exercise which no longer exists. They can be ignored.
+
+// const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+
+// const firstNames =  fullNames.map(function(name){
+//   return name.first;
+// })
