2b9b8f6c346429fe241774d7e8fb22e2f186726d
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-09 11:50:07 +0530

    New Features Completed

--- week1-4/javaScript/newFeatures/app.js
+++ week1-4/javaScript/newFeatures/app.js
@@ -13,56 +13,56 @@
 
 //     OLD WAY   //
 
-// function multiply(a,b){
-//     b = typeof b !== 'undefined' ? b: 1;
-//     return a * b; 
-// }
+function multiply(a,b){
+    b = typeof b !== 'undefined' ? b: 1;
+    return a * b; 
+}
 
-// console.log(multiply(12));
+console.log(multiply(12));
 
 
-// function rollDice(numSides) {
-//   if(numSides === undefined){
-//     numSides = 6;
-//   }
-//   return Math.floor(Math.random() * numSides) + 1; 
-// }
+function rollDice(numSides) {
+  if(numSides === undefined){
+    numSides = 6;
+  }
+  return Math.floor(Math.random() * numSides) + 1; 
+}
 
-// console.log(rollDice());
+console.log(rollDice());
 
 
 //    NEW WAY   //
 
-// function multiply( a, b = 1){
-//   return a * b;
-// }
+function multiply( a, b = 1){
+  return a * b;
+}
 
-// console.log(multiply(2));
-// console.log(multiply(2,20));
+console.log(multiply(2));
+console.log(multiply(2,20));
 
 
-// function rollDice(numSides = 20) {
-//   return Math.floor(Math.random() * numSides) + 1; 
-// }
+function rollDice(numSides = 20) {
+  return Math.floor(Math.random() * numSides) + 1; 
+}
 
-// console.log(rollDice());
+console.log(rollDice());
 
-// function greet (person, msg ="hey there", punc='!') {
-//       console.log( ` ${msg} , ${person}${punc}`);
-// }
+function greet (person, msg ="hey there", punc='!') {
+      console.log( ` ${msg} , ${person}${punc}`);
+}
 
 // *****                END                       *****//
 
 //        SPREAD in Funciton Calls                    //
 
-// const nums = [13,4,5,21,3,31,44444444];
+const nums = [13,4,5,21,3,31,44444444];
 
-// console.log(Math.max(...nums));
+console.log(Math.max(...nums));
 
-// console.log("asd" , 'as','asada','dadsadasda');
+console.log("asd" , 'as','asada','dadsadasda');
 
-// console.log('hello');
-// console.log(... 'hello');
+console.log('hello');
+console.log(... 'hello');
 
 // just adding the spread syntax (... element/array) we can spread the and iterable element or array.
 
@@ -70,62 +70,62 @@
 
 //        SPREAD in Array literals                    //
 
-// const cats = ['blue','Scout','Rocket'];
-// const dogs = ['Rusty', 'batatya'];
+const cats = ['blue','Scout','Rocket'];
+const dogs = ['Rusty', 'batatya'];
 
-// const allPets = [...cats, ...dogs];  // combining two arrays 
+const allPets = [...cats, ...dogs];  // combining two arrays 
 
-// const allPets1 = [1,2,3,...cats, ...dogs,'habibi'];  // combinig two arrays and pushing new elements
+const allPets1 = [1,2,3,...cats, ...dogs,'habibi'];  // combinig two arrays and pushing new elements
 
-// const hello = ["helloa"];
-// console.log(...hello[0]); // accessing string inside an array
+const hello = ["helloa"];
+console.log(...hello[0]); // accessing string inside an array
 
 // *****                END                       *****//
 
 //               SPREAD in Objects                   //
 
-// const feline = { legs: 4, family: 'Felidae'};
-// const canine = { family: 'Caninae' , furry: true };
+const feline = { legs: 4, family: 'Felidae'};
+const canine = { family: 'Caninae' , furry: true };
 
-// const dog = {...canine, isPet: true };
+const dog = {...canine, isPet: true };
 
-// const lion = { ...feline, genus: 'Panthera'};
+const lion = { ...feline, genus: 'Panthera'};
 
-// const catDog = { ...feline, ...canine};
+const catDog = { ...feline, ...canine};
 
-// const dataFromForm = { 
-//           email : 'andraa@gmail.com',
-//           password : 'ss@das11',
-//           username : 'fakename'
-// }
+const dataFromForm = { 
+          email : 'andraa@gmail.com',
+          password : 'ss@das11',
+          username : 'fakename'
+}
 
-// const newUser = {...dataFromForm, id:222, isAdmin: true };
+const newUser = {...dataFromForm, id:222, isAdmin: true };
 
 
 // *****                END                       *****//
 
 //                     REST Params                   //
 
-// function sum() {
-//   return arguments.reduce((x,y) =>{
-//     return x + y;
-//   })
-// } this will not work
+function sum() {
+  return arguments.reduce((x,y) =>{
+    return x + y;
+  })
+}    //  this will not work
 
 //Rest Params
-// function sum(...nums){
+function sum(...nums){
 
-//   return nums.reduce((total,el) => total + el );
+  return nums.reduce((total,el) => total + el );
 
-// }
+}
 
-// function raceResults(gold,silver,...everyoneElse){
-//   console.log(`GOLD medal goes to: ${gold}`);
-//   console.log(`SILVER medal goes to: ${silver}`);
-//   console.log(`And thanks to everyone else : ${everyoneElse}`)
-// }
+function raceResults(gold,silver,...everyoneElse){
+  console.log(`GOLD medal goes to: ${gold}`);
+  console.log(`SILVER medal goes to: ${silver}`);
+  console.log(`And thanks to everyone else : ${everyoneElse}`)
+}
 
-// raceResults('Dheeraj','manish','priyanka','pranay','mummy','pappa');
+raceResults('Dheeraj','manish','priyanka','pranay','mummy','pappa');
 
 
 // *****                END                       *****//
@@ -133,17 +133,17 @@
 //                     Destructuring Arrays                   //
 
 
-// const scores = [929321,899341,888336,772739,543671,243567,111934];
+const scores = [929321,899341,888336,772739,543671,243567,111934];
 
-// const [gold, silver, bronze,...everyoneElse] = scores;
+const [gold, silver, bronze,...everyoneElse] = scores;
 
-// const raceResults = [ 'Dheeraj','Manish', 'Priyanka','dheerya','hagrya'];
+const raceResults = [ 'Dheeraj','Manish', 'Priyanka','dheerya','hagrya'];
 
-// const [gold, silver , bronze, ...everyoneElse] = raceResults;
+const [gold, silver , bronze, ...everyoneElse] = raceResults;
 
 // instead of writing we can use destructuring arrays concepts.were we can give the arrays a distinct variable.
 
-// const gold = scores[0];
+const gold = scores[0];
 
 // *****                END                       *****//
 
@@ -151,42 +151,42 @@
 //                     Destructuring Objects                  //
 
 
-// const user =  {
-//       email : 'patildheeraj56@yyy.com',
-//       password : 'sdadasd',
-//       firstName : 'Dheeraj',
-//       lastName : 'Patil',
-//       born : 1995,
-//       died : 'Not yet',
-//       bio : 'Currently AML analyst, working for hudl',
-//       city : 'Vasai',
-//       state : 'Maharashtra'
-// }
+const user =  {
+      email : 'patildheeraj56@yyy.com',
+      password : 'sdadasd',
+      firstName : 'Dheeraj',
+      lastName : 'Patil',
+      born : 1995,
+      died : 'Not yet',
+      bio : 'Currently AML analyst, working for hudl',
+      city : 'Vasai',
+      state : 'Maharashtra'
+}
 
-// const firstName = user.firstName;
-// const lastName = user.lastName;
-// const email = user.email;
+const firstName = user.firstName;
+const lastName = user.lastName;
+const email = user.email;
 
-// const {email, firstName, lastName,bio } =  user;
+const {email, firstName, lastName,bio } =  user;
 
 // renaming and object property to a new one with destructuring objects
 
-// const { born : birthYear, died : deathYear} = user;
+const { born : birthYear, died : deathYear} = user;
 
 
-// const user2 = {
-//           email : 'patilp56@yyy.com',
-//           password : 'sdadasd',
-//           firstName : 'Priyanka',
-//           lastName : 'Patil',
-//           born : 1995,
-//           city : 'KOkan',
-//           state : 'Kankavali'
-// }
+const user2 = {
+          email : 'patilp56@yyy.com',
+          password : 'sdadasd',
+          firstName : 'Priyanka',
+          lastName : 'Patil',
+          born : 1995,
+          city : 'KOkan',
+          state : 'Kankavali'
+}
 
 // givin a default value to an object propety which is not in the object
 
-// const {died ='N/A', born, bio} = user2;
+const {died ='N/A', born, bio} = user2;
 
 
 // *****                END                       *****//
