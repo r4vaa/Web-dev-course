ad0792496f6486b26e3d4758c1cb2e9915f31423
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-13 20:09:11 +0530

    properties and methods

--- week1-4/javaScript/DOM/Manipulating/app.js
+++ week1-4/javaScript/DOM/Manipulating/app.js
@@ -1,12 +1,22 @@
-const allLinks = document.querySelectorAll('a');
+// const allLinks = document.querySelectorAll('a');
+
+// // for (let link of allLinks) {
+// //     link.innerText = 'I AM A LINK!!!!'
+// // }
+
 
 // for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
+ 
+// document.querySelector('h1').innerText selects only the text
+
+// const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks){
+//     link.innerText = 'I AM A LINK!!'; 
 // }
 
-
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+// document.querySelector('h1').innerHTML = '<i>HELLO</i>';  gives all the content with html, add elements
