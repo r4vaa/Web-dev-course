d0e89d730564ecca198e7f0584c22f828d64d490
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-16 22:56:05 +0530

    changing Styles

--- week1-4/javaScript/DOM/Manipulating/app.js
+++ week1-4/javaScript/DOM/Manipulating/app.js
@@ -39,11 +39,74 @@
 
 
 // const firstLink = document.querySelector('a');
-// undefined
+
 // firstLink.setAttribute('href','https://www.google.com');
-// undefined
+
 // firstLink.getAttribute('href');
 // 'https://www.google.com'
 // firstLink.href
 // 'https://www.google.com/'
 // document.querySelectorAll('input')[1]
+
+
+// //             Changing Styles                   //
+
+
+
+// const h1 = document.querySelector('h1');
+
+// h1.style.color
+
+// h1.style.color = 'green';
+
+// h1.style.fontSize = '3em';
+
+// h1.style.border = '2px solid pink';
+
+
+
+
+// const allLinks = document.querySelectorAll('a');
+
+// for(let link of allLinks){
+//   link.style.color = 'rgb(0,108,134)';
+//   link.style.textDecorationColor = 'blue';
+//   link.style.textDecorationStyle = 'wavy';
+//   link.style.textAlign
+// }
+
+
+
+// const h1 = document.querySelector('h1');
+
+// h1.style.fontSize
+
+// window.getComputedStyle(h1)
+
+// window.getComputedStyle(h1).fontSize
+
+// window.getComputedStyle(h1).fontSize;
+
+// window.getComputedStyle(h1).color;
+
+// window.getComputedStyle(h1).color = 'red';
+
+// window.getComputedStyle(h1).marginLeft
+
+// window.getComputedStyle(h1).margin
+
+// parseInt(window.getComputedStyle(h1).fontSize);
+
+
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+// const spans = ['r','a','i','n','b','o','w'];
+
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+// for(let i in colors){
+   
+//     console.log( spans[i] = colors[i]);
+    
+// }
