d83f1d4c8ba1a77d2d20f4dd84db5e0a941feada
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-16 16:32:35 +0530

    attributes

--- week1-4/javaScript/DOM/Manipulating/app.js
+++ week1-4/javaScript/DOM/Manipulating/app.js
@@ -20,3 +20,30 @@
 // }
 
 // document.querySelector('h1').innerHTML = '<i>HELLO</i>';  gives all the content with html, add elements
+
+
+
+// document.querySelector('#banner');
+
+// document.querySelector('#banner').id;
+// 'banner'
+// document.querySelector('#banner').id = 'whoops';
+// 'whoops'
+// document.querySelector('#whoops').id = 'banner';
+// 'banner'
+
+// document.querySelector('#banner').src;
+
+// document.querySelector('a').href;
+// document.querySelector('a').title;
+
+
+// const firstLink = document.querySelector('a');
+// undefined
+// firstLink.setAttribute('href','https://www.google.com');
+// undefined
+// firstLink.getAttribute('href');
+// 'https://www.google.com'
+// firstLink.href
+// 'https://www.google.com/'
+// document.querySelectorAll('input')[1]
