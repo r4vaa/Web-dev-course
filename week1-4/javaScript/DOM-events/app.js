304655ec0fa2e556299152b2e2ff72482d45abb1
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-18 15:57:51 +0530

    addEventListener

--- week1-4/javaScript/DOM-events/app.js
+++ week1-4/javaScript/DOM-events/app.js
@@ -7,10 +7,10 @@
 //   console.log("i Hope it worked!");
 // }
 
-// function scream(){
-//   console.log('AAAAA');
-//   console.log('stop touching me');
-// }
+function scream(){
+  console.log('AAAAA');
+  console.log('stop touching me');
+}
 
 // btn.onmouseenter = scream;
 
@@ -22,3 +22,28 @@
 // document.querySelector('h1').onclick = () => { 
 //     alert('you clicked me!')
 // }
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function() {
+      alert('CLICKED');
+});
+
+function dontScream(){
+  console.log('dontScream');
+}
+
+btn3.addEventListener('click', scream);
+btn3.addEventListener('click', dontScream);
+
+//practice
+
+// const helloBtn = document.querySelector('#hello');
+// const byeBtn = document.querySelector('#goodbye');
+
+// helloBtn.addEventListener('click',function(){
+//     console.log('hello');
+// })
+
+// byeBtn.addEventListener('click',function(){
+//     console.log('goodbye')
+// })
