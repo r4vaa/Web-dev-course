33fbfa7776019b01c906e99c1470eb6eb38a3a15
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-18 15:42:29 +0530

    onclick

--- week1-4/javaScript/DOM-events/app.js
+++ week1-4/javaScript/DOM-events/app.js
@@ -1,24 +1,24 @@
 //           DOM Events              //
 
-const btn = document.querySelector("#v2");
+// const btn = document.querySelector("#v2");
 
-btn.onclick = function(){
-  console.log('You clicked me!');
-  console.log("i Hope it worked!");
-}
+// btn.onclick = function(){
+//   console.log('You clicked me!');
+//   console.log("i Hope it worked!");
+// }
 
-function scream(){
-  console.log('AAAAA');
-  console.log('stop touching me');
-}
+// function scream(){
+//   console.log('AAAAA');
+//   console.log('stop touching me');
+// }
 
-btn.onmouseenter = scream;
+// btn.onmouseenter = scream;
 
 
 // document.querySelector('h1').onclick = function () {
 //   alert('you clicked me');
 // }
 
-document.querySelector('h1').onclick = () => { 
-    alert('you clicked me!')
-}
+// document.querySelector('h1').onclick = () => { 
+//     alert('you clicked me!')
+// }
