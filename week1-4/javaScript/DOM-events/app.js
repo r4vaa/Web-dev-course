605d0c92606bbf8370ec8829e43b56d394d6641f
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-18 10:29:24 +0530

    onclick property

--- week1-4/javaScript/DOM-events/app.js
+++ week1-4/javaScript/DOM-events/app.js
@@ -1,2 +1,24 @@
 //           DOM Events              //
 
+const btn = document.querySelector("#v2");
+
+btn.onclick = function(){
+  console.log('You clicked me!');
+  console.log("i Hope it worked!");
+}
+
+function scream(){
+  console.log('AAAAA');
+  console.log('stop touching me');
+}
+
+btn.onmouseenter = scream;
+
+
+// document.querySelector('h1').onclick = function () {
+//   alert('you clicked me');
+// }
+
+document.querySelector('h1').onclick = () => { 
+    alert('you clicked me!')
+}
