1c0df1720d729562f5da3e83e9a672113610364f
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-19 12:04:34 +0530

    shelter Form

--- week1-4/javaScript/DOM-events/PreventDefault/app.js
+++ week1-4/javaScript/DOM-events/PreventDefault/app.js
@@ -1,5 +1,13 @@
 const shelterForm = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats');
+
 shelterForm.addEventListener('submit',function(e){
-    e.preventDefault();
-    console.log("Submitted");
+      e.preventDefault();
+      const catNames =  input.value;
+      const newLi = document.createElement('li');
+      newLi.innerText = catNames;
+      list.append(newLi);
+      input.value = '';
+      
 })
