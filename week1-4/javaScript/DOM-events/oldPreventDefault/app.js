4568fe281c08a316435cf7b19b3fc840c65a60bd
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-23 11:07:09 +0530

    change and input (live text change)

--- week1-4/javaScript/DOM-events/oldPreventDefault/app.js
+++ week1-4/javaScript/DOM-events/oldPreventDefault/app.js
@@ -1,28 +1,28 @@
-const tweetForm = document.querySelector('#tweetForm');
-const tweets = document.querySelector('#tweets');
+// const tweetForm = document.querySelector('#tweetForm');
+// const tweets = document.querySelector('#tweets');
 
-tweetForm.addEventListener('submit',function(e){
-  e.preventDefault();
-  const userNameInput = tweetForm.elements.username;
-  const tweetInput = tweetForm.elements.tweet ;
-  addTweet(userNameInput.value , tweetInput.value);
-  userNameInput.value = '';
-  tweetInput.value= '';  
-  })
+// tweetForm.addEventListener('submit',function(e){
+//   e.preventDefault();
+//   const userNameInput = tweetForm.elements.username;
+//   const tweetInput = tweetForm.elements.tweet ;
+//   addTweet(userNameInput.value , tweetInput.value);
+//   userNameInput.value = '';
+//   tweetInput.value= '';  
+//   })
 
-const addTweet = (username, tweet) =>{
+// const addTweet = (username, tweet) =>{
 
-  const newTweet = document.createElement('li');
-  const bTag = document.createElement('b');
-  const pTag = document.createElement('p');
+//   const newTweet = document.createElement('li');
+//   const bTag = document.createElement('b');
+//   const pTag = document.createElement('p');
 
-  bTag.innerText = username;
-  newTweet.append(bTag);
-  pTag.innerText = tweet;
-  newTweet.append(pTag);
-  tweets.append(newTweet);
+//   bTag.innerText = username;
+//   newTweet.append(bTag);
+//   pTag.innerText = tweet;
+//   newTweet.append(pTag);
+//   tweets.append(newTweet);
 
-}
+// }
 
 // practice
 
@@ -41,3 +41,36 @@ const addTweet = (username, tweet) =>{
 
  
 // })
+
+
+// Change and inpunt events
+
+// const input = document.querySelector('input');
+
+// input.addEventListener('change',function(e){
+//   console.log('input trigged');
+// })
+
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
+
+// input.addEventListener('input',function(e){
+
+//   h1.innerText = input.value;
+  
+// })
+
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+
+input.addEventListener('input',function(e){
+      e.preventDefault();
+      const inputValue = input.value;
+     
+     if(inputValue === ''){
+       h1.innerText = 'Enter Your Username'
+     }else{
+      h1.innerText = `Welcome, ${inputValue}`;
+     }
+
+})
