708a35d8b81641c54b4c7ec5c01d5dcfc160ade0
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-23 10:46:47 +0530

    practice of preventDefault

--- week1-4/javaScript/DOM-events/oldPreventDefault/app.js
+++ week1-4/javaScript/DOM-events/oldPreventDefault/app.js
@@ -23,3 +23,21 @@ const addTweet = (username, tweet) =>{
   tweets.append(newTweet);
 
 }
+
+// practice
+
+// const form = document.querySelector('#form');
+// const ulList = document.querySelector('#list');
+
+// form.addEventListener('submit',function(e){
+//   e.preventDefault();
+//   const productNameInput = form.elements.product;
+//   const quantityInput = form.elements.qty;
+//   const newLi = document.createElement('li');
+//   ulList.appendChild(newLi);
+//   newLi.innerText=`${productNameInput.value} ${quantityInput.value}`;
+//   productNameInput.value = '';
+//   quantityInput.value = '';
+
+ 
+// })
