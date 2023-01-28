edc3491cfa5d5ba3b68d350b8b6fea58244148b4
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-28 10:52:30 +0530

    delayedColor with promises

--- week1-4/javaScript/AsyncJavaScript/creatingPromises/app.js
+++ week1-4/javaScript/AsyncJavaScript/creatingPromises/app.js
@@ -1,24 +1,45 @@
-const fakeRequest = (url) => {
- 	return new Promise ((resolve, reject) => {
-		const rand = Math.random();
-		setTimeout(()=> {
-			if(rand < 0.7){
-				resolve('Your data is here');
-			}else
-			{
-				reject('OHH no , no data');
-			}
+	//  Creating a new Promise
+
+// const fakeRequest = (url) => {
+//  	return new Promise ((resolve, reject) => {
+// 		const rand = Math.random();
+// 		setTimeout(()=> {
+// 			if(rand < 0.7){
+// 				resolve('Your data is here');
+// 			}else
+// 			{
+// 				reject('OHH no , no data');
+// 			}
 			
-		},1000);
-  	})
+// 		},1000);
+//   	})
+// }
+
+// fakeRequest('dog/pag1')
+// 	.then((data) => {
+// 		console.log('resolved');
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log('sorry');
+// 		console.log(err)
+// 	})
+
+const delayedColor = (color, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() =>{
+			document.body.style.backgroundColor = color;
+			resolve();
+		}, delay)
+	})
 }
 
-fakeRequest('dog/pag1')
-	.then((data) => {
-		console.log('resolved');
-		console.log(data);
-	})
-	.catch((err) => {
-		console.log('sorry');
-		console.log(err)
-	})
+delayedColor('red',1000)
+	.then(() => delayedColor('orange',1000))
+	.then(() => delayedColor('green',1000))
+	.then(() => delayedColor('blue',1000))
+	.then(() => delayedColor('teal',1000))
+	.then(() => delayedColor('grey',1000))
+	.then(() => delayedColor('indigo',1000))
+	.then(() => delayedColor('violet',1000))
+	
