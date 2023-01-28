ea9b1f9b2fa60b39efdbd2e9e0ece6f59e6a69b6
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-28 11:34:16 +0530

    async and await

--- week1-4/javaScript/AsyncJavaScript/asyncFunc/app.js
+++ week1-4/javaScript/AsyncJavaScript/asyncFunc/app.js
@@ -19,19 +19,60 @@
 //     })
 
 
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if(password === 'corgifeetarecute') return 'welcome!'
-    throw 'Invalid Password'
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing Credentials'
+//     if(password === 'corgifeetarecute') return 'welcome!'
+//     throw 'Invalid Password'
+
+// }
+
+// login('dheeraj','corgifeetarecute' )
+//     .then(msg => {
+//         console.log('logged in');
+//         console.log(msg)
+//     })
+//     .catch(err => {
+//         console.log('ERROR!!');
+//         console.log(err)
+//     })
+
+
+const delayedColor = (color, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() =>{
+			document.body.style.backgroundColor = color;
+			resolve();
+		}, delay)
+	})
+}
+
+// delayedColor('red',1000)
+// 	.then(() => delayedColor('orange',1000))
+// 	.then(() => delayedColor('green',1000))
+// 	.then(() => delayedColor('blue',1000))
+// 	.then(() => delayedColor('teal',1000))
+// 	.then(() => delayedColor('grey',1000))
+// 	.then(() => delayedColor('indigo',1000))
+// 	.then(() => delayedColor('violet',1000))
+	
+async function rainbow(){
+    await delayedColor('red',1000);
+    await delayedColor('orange',1000);
+    await delayedColor('yellow',1000);
+    await delayedColor('teal',1000);
+    await delayedColor('grey',1000);
+    await delayedColor('indigo',1000);
+    await delayedColor('blue',1000);
+    await delayedColor('violet',1000);
+    return 'ALL Done!s'
 
 }
 
-login('dheeraj','hhh' )
-    .then(msg => {
-        console.log('logged in');
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log('ERROR!!');
-        console.log(err)
-    })
+// rainbow().then(() => console.log('END of Rainbow!'))
+
+async function printRainbow(){
+    await rainbow();
+    console.log('END of Rainbowssss!');
+}
+
+printRainbow();
