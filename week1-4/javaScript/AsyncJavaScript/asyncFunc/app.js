49108cfc1e1d86286e25466cd6cf1d9066eef1a4
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-01-28 11:45:07 +0530

    handling errors with async function using try catch method

--- week1-4/javaScript/AsyncJavaScript/asyncFunc/app.js
+++ week1-4/javaScript/AsyncJavaScript/asyncFunc/app.js
@@ -55,24 +55,53 @@ const delayedColor = (color, delay) => {
 // 	.then(() => delayedColor('indigo',1000))
 // 	.then(() => delayedColor('violet',1000))
 	
-async function rainbow(){
-    await delayedColor('red',1000);
-    await delayedColor('orange',1000);
-    await delayedColor('yellow',1000);
-    await delayedColor('teal',1000);
-    await delayedColor('grey',1000);
-    await delayedColor('indigo',1000);
-    await delayedColor('blue',1000);
-    await delayedColor('violet',1000);
-    return 'ALL Done!s'
+// async function rainbow(){
+//     await delayedColor('red',1000);
+//     await delayedColor('orange',1000);
+//     await delayedColor('yellow',1000);
+//     await delayedColor('teal',1000);
+//     await delayedColor('grey',1000);
+//     await delayedColor('indigo',1000);
+//     await delayedColor('blue',1000);
+//     await delayedColor('violet',1000);
+//     return 'ALL Done!s'
 
+// }
+
+// // rainbow().then(() => console.log('END of Rainbow!'))
+
+// async function printRainbow(){
+//     await rainbow();
+//     console.log('END of Rainbowssss!');
+// }
+
+// printRainbow();
+
+
+// Handling errors in Async functions
+
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
 }
 
-// rainbow().then(() => console.log('END of Rainbow!'))
-
-async function printRainbow(){
-    await rainbow();
-    console.log('END of Rainbowssss!');
+async function makeTwoRequests(){
+    try{
+        let data1 = await fakeRequestPromise('/page1')
+        console.log(data1);
+        let data2 = await fakeRequestPromise('/page2')
+        console.log(data2);
+    }catch(e){
+        console.log('Errorrr');
+        console.log('this is your error :', e);
+    }
+    
 }
-
-printRainbow();
