aacaa9ff3ddf8aacb48d55e8e579048f1328deb7
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-01 19:19:48 +0530

    dad joke

--- week1-4/javaScript/Axios/app.js
+++ week1-4/javaScript/Axios/app.js
@@ -9,15 +9,40 @@
 // })
 
 
-const getStarWarPerson = async(id) => {
-    try{
-        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-        console.log(res.data);
-    }catch(e){
-        console.log("ERROR",e)
-    }
-    } 
+// const getStarWarPerson = async(id) => {
+//     try{
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data.name, res.data.birth_year);
+//     }catch(e){
+//         console.log("ERROR",e)
+//     }
+//     } 
     
 
-getStarWarPerson(10);
-getStarWarPerson(1);
+// getStarWarPerson(10);
+// getStarWarPerson(1);
+
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+
+
+const addNewJoke = async() =>{
+        const joke = await getDadJoke();
+        const newLI = document.createElement('li');
+        newLI.append(joke)
+        jokes.append(newLI)  
+}
+
+const getDadJoke = async() => {
+    try{
+    const config = { headers: { Accept: 'application/json' }}
+    const res = await axios.get('https://icanhazdadjoke.com/',config);
+    return res.data.joke
+    }catch(e){
+    return `NO JOKES AVAILABLE SORRY :(`
+    }
+}
+
+button.addEventListener('click', addNewJoke)
