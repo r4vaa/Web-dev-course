d8350d3a0c085f5395ea1b581641ac53107e6ec7
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-02 18:03:44 +0530

    Factory Functions

--- week1-4/javaScript/PCO/prototypes/app.js
+++ week1-4/javaScript/PCO/prototypes/app.js
@@ -10,3 +10,31 @@
 
 
 //   OOP
+
+//  Factory Function
+
+
+
+
+
+
+function makeColor(r,g,b){
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function() {
+        const { r, g, b } = this;
+        return `rgb(${r},${g},${b})`;
+    }
+    
+    color.hex = function() {
+        const { r, g, b } = this;
+        return '#' +((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
+    return color;
+}
+
+const firstColor = makeColor(11,22,33);
+firstColor.hex()
