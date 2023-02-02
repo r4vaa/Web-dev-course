ae987318f74319c92a0743a89397826ab95caf6f
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-02 17:04:35 +0530

    OOP

---
+++ week1-4/javaScript/PCO/prototypes/app.js
@@ -0,0 +1,12 @@
+// Prototype
+
+// String.prototype.yell = function(){
+//     return `OMG!!!  ${this.toUpperCase()} !!!!`
+// }
+
+// Array.prototype.push = function(){
+//     return `Sorry Cant delete that ${this}element`;
+// }
+
+
+//   OOP
