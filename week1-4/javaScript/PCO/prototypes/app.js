5312da5cdf746a6f88b1050dddae0647508f7379
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-04 10:48:28 +0530

    Constructor function

--- week1-4/javaScript/PCO/prototypes/app.js
+++ week1-4/javaScript/PCO/prototypes/app.js
@@ -18,23 +18,56 @@
 
 
 
-function makeColor(r,g,b){
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function() {
-        const { r, g, b } = this;
-        return `rgb(${r},${g},${b})`;
-    }
+// function makeColor(r,g,b){
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function() {
+//         const { r, g, b } = this;
+//         return `rgb(${r},${g},${b})`;
+//     }
     
-    color.hex = function() {
-        const { r, g, b } = this;
-        return '#' +((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
+//     color.hex = function() {
+//         const { r, g, b } = this;
+//         return '#' +((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
 
-    return color;
+//     return color;
+// }
+
+// const firstColor = makeColor(11,22,33);
+// firstColor.hex() 
+
+
+// constructor function
+// Creates a blank, plain JavaScript Object:
+// Links(sets the constructor of) this object to another object;
+// Passes the newly created Object from step 1 as the this context;
+// Returns this if the function doesn't return its own object.
+
+
+function Color(r,g,b){
+    this.r = r;
+    this.g = g;
+    this.b = b;
 }
 
-const firstColor = makeColor(11,22,33);
-firstColor.hex()
+Color.prototype.rgb = function(){
+    const { r, g, b} = this;
+    return `rgb(${r},${g},${b})`;
+}
+
+Color.prototype.hex = function() {
+            const { r, g, b } = this;
+            return '#' +((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+        }
+    
+Color.prototype.rgba = function(a = 1.0) {
+        const { r , g, b } = this;
+        return `rgba(${r},${g},${b},${a})`;
+}
+
+
+const color1 = new Color(24,111,12);
+const color2 = new Color(11,255,11);
