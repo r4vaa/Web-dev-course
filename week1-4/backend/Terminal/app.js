a0b313c240d1a9e966dc6ff5593f1e879fe1a1e2
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-05 12:31:14 +0530

    git commands

---
+++ week1-4/backend/Terminal/app.js
@@ -0,0 +1,15 @@
+//
+// ## Commands used
+// - __ls__ -  shows us a list of directories, in our current directory.
+// - __cd__ - change directories.
+// - __mkdir__ - make a new directory, we can make more than one directories at one go.
+// - __rmdir__ - remove the specified directory.
+// - __rm__ - removes the specified file.
+// - __rm -rf__ - remove recursive force
+// - __touch__ - create files in directories __(touch index.html)__
+// - __pwd__ - shows the current path that we are on.
+// - __Man/Flags__ -man for mac users,for windows users its, _command_ --help __(ls --help)__
+
+// Relative vs abosulte paths
+// - An absolute path is defined as specifying the location of a file or directory from the root directory(/). In other words,we can say that **an absolute path is a complete path from start of actual file system from / directory.** **Relative path is defined as the path related to the present working directly(pwd)**
+// - Relative path is defined as the path related to the present working directly(pwd). It starts at your current directory and **never starts with a /** .
