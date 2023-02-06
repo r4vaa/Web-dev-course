75926068cd20309ea00d6348de555b74de6b9aed
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-06 09:27:18 +0530

    module exports

---
+++ week1-4/backend/modules_npm/app.js
@@ -0,0 +1,15 @@
+const {square, PI,add}= require("./math");
+
+const cats = require('./shelter')
+
+
+
+console.log("required an entire directory",cats)
+
+// console.log(PI);
+
+// console.log(square(9));
+
+// console.log(add(7,9));
+
+// console.log(PI , square (9));
---
+++ week1-4/backend/modules_npm/math.js
@@ -0,0 +1,30 @@
+const add = (x, y) => x + y;
+
+const PI = 3.149;
+
+const square = (x) => x * x;
+
+
+
+
+
+// module.exports.add = (x, y) => x + y;
+
+// module.exports.PI = 3.149;
+
+// module.exports.square = (x) => x * x;
+
+
+const math = {
+    add : add ,
+    PI : PI ,
+    square : square
+}
+
+
+module.exports = math
+
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
