d9213744f098506ed3e241220aaec924fdb76da5
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-06 16:31:00 +0530

    res.send()

--- week1-4/backend/npm/firstApp/index.js
+++ week1-4/backend/npm/firstApp/index.js
@@ -1,8 +1,11 @@
 const express =  require('express');
 const app = express()
 
-app.use( () => {
+app.use( (req, res) => {
     console.log("WE GOT A REQUEST");
+    // res.send("hello, we got your reques! this is our response")
+    // res.send({color: 'red'})
+    res.send('<h1>this is my webpage</h1>')
 })
 
 app.listen(3000, () => {
