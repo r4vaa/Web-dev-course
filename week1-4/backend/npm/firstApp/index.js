cf2cdfc023f876d7b26b667a655c59c5946209a4
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-06 16:58:48 +0530

    express routing basics

--- week1-4/backend/npm/firstApp/index.js
+++ week1-4/backend/npm/firstApp/index.js
@@ -1,12 +1,42 @@
 const express =  require('express');
+
 const app = express()
 
-app.use( (req, res) => {
-    console.log("WE GOT A REQUEST");
-    // res.send("hello, we got your reques! this is our response")
-    // res.send({color: 'red'})
-    res.send('<h1>this is my webpage</h1>')
+// USED TO GET ANY REQ AND MATCHES EVERYTHING THAT WE GET
+// app.use( (req, res) => {
+//     console.log("WE GOT A REQUEST");
+//     // res.send("hello, we got your reques! this is our response")
+//     // res.send({color: 'red'})
+//     res.send('<h1>this is my webpage</h1>')
+// })
+
+//  GETS THE REQUEST FOR HOMEPAGE OF /  AND GIVES A RESPONSE OF HOMEPAGE
+app.get('/',(req, res) => {
+    res.send('this is a home page');
 })
+
+//POST THE REQUEST THAT WE GET FROM CLIENT
+app.post('/cats', (req,res) => {
+    res.send('this is a post request for cats')
+})
+
+
+// GET THE REQUEST FROM CLIENT ABOUTS /CATS AND SEND A RESPONSE OF MEOW!
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!');
+})
+
+
+// GET THE REQUEST FROM CLIENT ABOUTS /DOGS AND SEND A RESPONSE OF WOOFF!
+app.get('/dogs', (req , res) => {
+    res.send('WOOOFFF');
+})
+
+// TAKE ANY REQUEST FROM THE CLIENT AND GIVES A SPECIFIC RESPONE
+app.get('*', (req, res) => {
+    res.send('WE DONT HAVE THIS TYPE OF URL, PLEASE CHECK')
+})
+
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!")
