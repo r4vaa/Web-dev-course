d149aa83cf77698176ba0e0d9eb22b662c81e4b8
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-08 16:37:03 +0530

    parsing the request body

--- week1-4/backend/REST/index.js
+++ week1-4/backend/REST/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response')
 })
 
 app.post('/tacos' , (req, res) => {
-    res.send('POST /tacos response')
+    const { meat , qty } = req.body
+    
+    res.send(`this is your order of ${qty} ${meat} tacos`)
 })
 
 app.listen(3000, () => {
