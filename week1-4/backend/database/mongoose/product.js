cd19647e0632d1bca3ed15ad926ecb11f3274d20
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-16 11:05:10 +0530

    error message and ENUM

--- week1-4/backend/database/mongoose/product.js
+++ week1-4/backend/database/mongoose/product.js
@@ -23,7 +23,7 @@ const productSchema = new mongoose.Schema( {
     price : {
         type :Number,
         required : true,
-        min : 0
+        min : [0 ,'Price must be positive DOdo!']
     },
     onSale : {
         type :  Boolean,
@@ -39,13 +39,24 @@ const productSchema = new mongoose.Schema( {
             type : Number,
             default : 0
         }
+    },
+    size : {
+        type : String ,
+        enum : ['S','M', 'L']
     }
 })
 
 const Product = mongoose.model('Product', productSchema);
 
+            // creating a new product //
+// // const bike = new Product( {name: 'Tire Pump', price : 20, categories : ['Cycling'], qty:{online: 10, inStore : 12}} );
+// bike.save()
 
-const bike = new Product( {name: 'Bike Helmet red', price : 10.50, categories : ['Cycling', 'Safety','black','green'], qty:{online: 10, inStore : 12}} );
+ 
+            // updating a product  //
+// Product.findOneAndUpdate({name : 'Tire Pump'}, { price : -9 }, {new : true, runValidators : true})
+
+const bike = new Product({name:'Cycling Jersey', price: 28.50, categories: ['Cycling'],size:'S' })
 bike.save()
 .then(data => {
     console.log("it Workeddd");
