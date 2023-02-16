4e0853f3937e9158d14245f1a80e5a901a157ab1
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-16 10:35:43 +0530

    adding objects and arrays in Schema

--- week1-4/backend/database/mongoose/product.js
+++ week1-4/backend/database/mongoose/product.js
@@ -17,27 +17,40 @@ async function main() {
 const productSchema = new mongoose.Schema( {
     name : {
         type : String,
-        required : true 
+        required : true,
+        maxlength : 20
     },
     price : {
         type :Number,
-        required : true
+        required : true,
+        min : 0
     },
     onSale : {
         type :  Boolean,
         default : false
+    },
+    categories : [String],
+    qty: {
+        online : {
+            type : Number,
+            default : 0
+        },
+        inStore : {
+            type : Number,
+            default : 0
+        }
     }
 })
 
 const Product = mongoose.model('Product', productSchema);
 
 
-// const bike = new Product( {name: 'Mountain Bike', price : 999, color: 'red'} );
-// bike.save()
-// .then(data => {
-//     console.log("it Workeddd");
-//     console.log(data);
-// }).catch(err => {
-//     console.log("ERROR");
-//     console.log(err)
-// })
+const bike = new Product( {name: 'Bike Helmet red', price : 10.50, categories : ['Cycling', 'Safety','black','green'], qty:{online: 10, inStore : 12}} );
+bike.save()
+.then(data => {
+    console.log("it Workeddd");
+    console.log(data);
+}).catch(err => {
+    console.log("ERROR");
+    console.log(err)
+})
