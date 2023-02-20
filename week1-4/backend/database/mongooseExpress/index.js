677b864436981ec984f981132f19f4aeae06868e
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-21 00:39:49 +0530

    filtering  by category

--- week1-4/backend/database/mongooseExpress/index.js
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -27,8 +27,15 @@ app.use(methodOverride('_method'))
 categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
-    const products =  await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if( category){
+        const products = await Product.find( { category })
+        res.render('products/index', { products , category })
+    }else {
+        const products =  await Product.find({})
+        res.render('products/index', { products , category : 'All' })
+    }
+    
 })
 
 app.get('/products/new' , (req ,res ) => {
