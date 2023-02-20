21363fcdd92eef388bf21859b0ba64cd88e1cab5
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-20 16:39:09 +0530

    products details

--- week1-4/backend/database/mongooseExpress/index.js
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -26,6 +26,13 @@ app.get('/products', async (req, res) => {
 })
 
 
+app.get('/products/:id' , async( req, res ) => { 
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product)
+    res.render('products/show' , { product })
+})
+
 
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000')
