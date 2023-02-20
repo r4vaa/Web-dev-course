5d5435b3a4bbd1b567e9e99e43e6f243355c190b
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-20 11:31:51 +0530

    creating a model

--- week1-4/backend/database/mongooseExpress/index.js
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -3,17 +3,19 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product =  require('./models/product');
+
 mongoose.set('strictQuery', false);
-main().catch(err => console.log(err));
-async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
- .then(() => {
- console.log('Mongoose Connection OPen')
- })
- .catch(err => {
- console.log('OH no !! Mongoose  ErroR!!!');
- console.log(err)
- })
-}
+    main().catch(err => console.log(err));
+async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+            .then(() => {
+            console.log('Mongoose Connection OPen')
+            })
+            .catch(err => {
+            console.log('OH no !! Mongoose  ErroR!!!');
+            console.log(err)
+            })
+            }
 
 app.set('view', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
---
+++ week1-4/backend/database/mongooseExpress/seeds.js
@@ -0,0 +1,74 @@
+
+const mongoose = require('mongoose');
+
+const Product =  require('./models/product');
+
+mongoose.set('strictQuery', false);
+main().catch(err => console.log(err));
+async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(() => {
+        console.log('Mongoose Connection OPen')
+    })
+    .catch(err => {
+        console.log('OH no !! Mongoose  ErroR!!!');
+        console.log(err)
+    })
+ }
+
+// const p = new Product ({
+//     name : 'Ruby GrapeFruit',
+//     price : 1.99,
+//     category : 'fruit'
+// })
+// p.save()
+//     .then(p => {
+//         console.log(p)
+//     })
+//     .catch(e => {
+//         console.log(e);
+//     })
+const seedProducts = [
+   
+    {
+        name : 'Fairy Eggplant',
+        price : 1.00,
+        category : 'vegetable'
+    },
+    {
+        name : 'Organic Goddess Melon',
+        price : 4.99,
+        category : 'fruit'
+    },
+    {
+        name : 'Organic Mini Seedless Watermelon',
+        price : 3.99,
+        category : 'fruit'
+    },
+    {
+        name : 'Organic Calery',
+        price : 1.59,
+        category : 'vegetable'
+    },
+    {
+        name : 'Chocolate Whole Milk',
+        price : 2.69,
+        category : 'dairy'
+    },
+     
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e =>{
+        console.log(e)
+    })
+
+
+
+
+
+
+
+// this is just for development purpose so that we create new products and check if it is working or not
