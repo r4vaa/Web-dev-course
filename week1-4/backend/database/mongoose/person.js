c7aead8e51227c71db4c6f9bfdf52616d576fc26
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-20 10:04:56 +0530

    mongoose middleware

--- week1-4/backend/database/mongoose/person.js
+++ week1-4/backend/database/mongoose/person.js
@@ -25,4 +25,15 @@ personSchema.virtual('fullName').get(function( ) {
     return `${this.first} ${this.last}`
 })
 
+personSchema.pre('save', async function() {
+    this.first = 'YO';
+    this.last = 'MAMA'
+    console.log("ABOUT TO SAVE !!!")
+
+})
+
+personSchema.post('save' , async function() {
+    console.log("JUST SAVED");
+})
+
 const Person = mongoose.model ( 'Person' , personSchema);
