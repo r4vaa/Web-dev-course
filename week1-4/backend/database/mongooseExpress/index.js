e5a9cee0b2154b09feb2bf0eadf30c35e08151aa
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-20 10:42:47 +0530

    setting up express + mongoose

---
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -0,0 +1,26 @@
+const express =  require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+main().catch(err => console.log(err));
+async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+ .then(() => {
+ console.log('Mongoose Connection OPen')
+ })
+ .catch(err => {
+ console.log('OH no !! Mongoose  ErroR!!!');
+ console.log(err)
+ })
+}
+
+app.set('view', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog',(req, res) => {
+    res.send('WOOFF');
+})
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT 3000')
+})
