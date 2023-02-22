ba3ee410db8d19818c9899f016fbe0eb6dc5436b
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-02-22 10:36:16 +0530

    Middleware Practice

--- week1-4/backend/middleware/index.js
+++ week1-4/backend/middleware/index.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const { ConnectionStates } = require('mongoose');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//     console.log('THIS IS MY FIRST MIDDLE Ware')
+//     next();
+//     console.log('this is my middle ware after 1st and 2nd')
+// })
+
+// app.use((req, res, next) => {
+//     console.log('THIS IS MY SECOND MIDDLE Ware')
+//     next();
+// })
+
+app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method , req.path);
+    next();
+})
 
 app.get('/', (req , res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('home PAGE!!')
 })
 
 app.get('/dogs',(req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('WOFF WOOF')
 })
 
