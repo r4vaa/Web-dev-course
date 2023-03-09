026edae4a269e06287c6cf86371cbe5345809858
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-09 20:06:19 +0530

    Finishing Touches

--- week1-4/backend/database/mongooseExpress/index.js
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -34,14 +34,17 @@ app.get('/farms' ,async (req, res ) => {
     res.render('farms/index', {farms});
 })
 
-app.get('/farms/:id', async(req, res) => {
-   const farm =  await Farm.findById(req.params.id)
-   res.render('farms/show', { farm })
-})
-
 app.get('/farms/new' , (req, res) => {
     res.render('farms/new')
 })
+
+app.get('/farms/:id', async(req, res) => {
+   const farm =  await Farm.findById(req.params.id).populate('products')
+   console.log(farm);
+   res.render('farms/show', { farm })
+})
+
+
 
 app.post('/farms' , async(req, res) => {
     const farm = new Farm(req.body);
@@ -50,9 +53,10 @@ app.post('/farms' , async(req, res) => {
 })
 
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
     const { id } = req.params
-    res.render('products/new', { categories, id})
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories,farm})
 })
 
 app.post('/farms/:id/products', async(req, res) => {
@@ -64,7 +68,7 @@ app.post('/farms/:id/products', async(req, res) => {
     product.farm = farm;
     await farm.save();
     await product.save();
-    res.send(farm)
+    res.redirect(`/farms/${id}`)
 })
 
 
@@ -114,11 +118,12 @@ app.post('/products' , wrapAsync(async(req , res , next)=> {
 app.get('/products/:id' , wrapAsync(async( req, res, next ) => { 
     
         const { id } = req.params;
-        const product = await Product.findById(id);
+        const product = await Product.findById(id).populate('farm','name')
         // console.log(product)
-        if(!product){
-         throw new AppError('Product Not Found', 404 )
-        }
+        // if(!product){
+        //  throw new AppError('Product Not Found', 404 )
+        // }
+        console.log(product)
         res.render('products/show' , { product })
    
     
