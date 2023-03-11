bda32f520c0e0015f5941b21234f2e1968f081d4
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-11 19:55:34 +0530

    Cookie parser Middleware

--- week1-4/backend/database/expressRouter/CookiesDemo/index.js
+++ week1-4/backend/database/expressRouter/CookiesDemo/index.js
@@ -1,9 +1,12 @@
+const cookieParser = require('cookie-parser');
 const express =  require('express');
 const app = express();
 
+app.use(cookieParser());
 
 app.get('/greet', (req, res) => {
-    res.send('HELLO')
+    const { name = 'No-name'}  = req.cookies
+    res.send(`Hey there ${name}`);
 })
 
 app.get('/setname' , (req, res ) => {
