beee0cc84f5e7aff6d0ea40120cd6a754b93148a
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-13 09:20:47 +0530

    creating middleware for flash

--- week1-4/backend/database/mongooseExpress/index.js
+++ week1-4/backend/database/mongooseExpress/index.js
@@ -36,9 +36,14 @@ app.use(methodOverride('_method'))
 
 // FARM ROUTES
 
+app.use((req, res, next) => {
+    res.locals.messages = req.flash('success');
+    next();
+})
+
 app.get('/farms' ,async (req, res ) => {
     const farms =  await Farm.find({});
-    res.render('farms/index', {farms , messages : req.flash('success') });
+    res.render('farms/index', {farms });
 })
 
 app.get('/farms/new' , (req, res) => {
