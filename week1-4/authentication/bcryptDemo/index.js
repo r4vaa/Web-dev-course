4dd3544c7c04a9c25111cb0391409ee453bf9821
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-14 11:59:19 +0530

    salt and has simultaneously

--- week1-4/authentication/bcryptDemo/index.js
+++ week1-4/authentication/bcryptDemo/index.js
@@ -1,12 +1,20 @@
 const bcrypt = require('bcrypt');
 
 // creating hashpassword with salt 
+// const hashPassword = async(pw) => {
+//     const salt = await bcrypt.genSalt(12);
+//     const hash = await bcrypt.hash( pw ,salt );
+//     console.log(hash);
+//     console.log(salt);
+// }
+
+// creating salt and hashPassword simultaneously
+
 const hashPassword = async(pw) => {
-    const salt = await bcrypt.genSalt(12);
-    const hash = await bcrypt.hash( pw ,salt );
+    const hash = await bcrypt.hash( pw ,12 );
     console.log(hash);
-    console.log(salt);
 }
+
 
 // compare the pass with hashedPassword
 
@@ -21,4 +29,4 @@ const login = async (pw , hashedPassword) =>{
 
 // hashPassword('monkey');
 
-login('monkey', '$2b$12$t5BK/mmJewquXVoaOWaK1eQvSxYo2wrmn9e6NNgpRH6SUXqG57yk1');
+login('monkey', '$2b$12$PNyjxoMKW48xxSBxI3CkF.SaIE4duDr9.84qWeVNoSy8I80TweMK.');// 
