6ea07d3c39c1c1df9d26d739a423f778adbdebd9
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-14 16:51:36 +0530

    creating hash password for user

---
+++ week1-4/authentication/AuthDemo/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+mongoose.set('strictQuery', false);
+    main().catch(err => console.log(err));
+    async function main() { await mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
+        .then(() => {
+        console.log('Database Connected')
+    })
+    .catch(err => {
+        console.log('OH no !! Mongoose ErroR!!!');
+        console.log(err)
+    })
+}
+
+app.set('view engine', 'ejs');
+app.set('views','views');
+
+app.use(express.urlencoded({ extended : true }))
+
+app.get('/', (req, res) => {
+    res.send('THIS IS HOME PAGE');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', async(req, res) => {
+    const { password , username} = req.body;
+    const hash = await bcrypt.hash(password , 12);
+    const user = new User( {
+        username,
+        password : hash
+    })
+    await user.save();
+    res.redirect('/')
+})
+
+app.get('/secret' , (req , res ) => [
+    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE ADMIN')
+])
+
+
+app.listen(3000, () => {
+    console.log('SERVING YOUR APP!');
+})
