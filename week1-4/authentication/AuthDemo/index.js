b3b31390c57f58a2c65f70436df7c2ac5bd01089
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-14 22:41:34 +0530

    Refactoring To Model Methods

--- week1-4/authentication/AuthDemo/index.js
+++ week1-4/authentication/AuthDemo/index.js
@@ -19,11 +19,21 @@ mongoose.set('strictQuery', false);
     })
 }
 
+
+
 app.set('view engine', 'ejs');
 app.set('views','views');
 
 app.use(express.urlencoded({ extended : true }))
 app.use(session({secret: 'notagoodsecret', saveUninitialized : true, resave : false}))
+
+const requireLogin = ((req, res , next) => {
+    if(!req.session.user_id){
+       return res.redirect('/login')
+    }
+    next();
+})
+
 
 app.get('/', (req, res) => {
     res.send('THIS IS HOME PAGE');
@@ -35,11 +45,8 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async(req, res) => {
     const { password , username} = req.body;
-    const hash = await bcrypt.hash(password , 12);
-    const user = new User( {
-        username,
-        password : hash
-    })
+    // const hash = await bcrypt.hash(password , 12);
+    const user = new User( { username,password })
     await user.save();
     req.session.user_id = user._id
     res.redirect('/')
@@ -51,10 +58,11 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async(req, res) => {
     const { username , password} = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password , user.password);
-    if(validPassword){
-        req.session.user_id = user._id
+    const foundUser = await User.findAndValidate(username, password)
+    // const user = await User.findOne({ username });
+    // const validPassword = await bcrypt.compare(password , user.password);
+    if(foundUser){
+        req.session.user_id = foundUser._id
         res.redirect('/secret')
     }else{
         res.redirect('/login');
@@ -62,16 +70,20 @@ app.post('/login', async(req, res) => {
 })
 
 app.post('/logout' , (req, res) => {
-    // req.session.user_id = null;
-    req.session.destroy();
+    req.session.user_id = null;
+    // req.session.destroy();
     res.redirect('/login');
 })
 
-app.get('/secret' , (req , res ) => {
-    if(!req.session.user_id){
-       return res.redirect('/login')
-    }
+app.get('/secret' ,requireLogin,(req , res ) => {
+    // if(!req.session.user_id){
+    //    return res.redirect('/login')
+    // }
     res.render('secret');     
+})
+
+app.get('/topSecret', requireLogin,( req , res ) => {
+    res.redirect('secret');
 })
 
 
