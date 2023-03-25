ed16056fd690a3dcf36bd2da030f74f0ab4757fd
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-25 15:02:19 +0530

    get, write,send methods

--- angelaYu/API/weatherapp/app.js
+++ angelaYu/API/weatherapp/app.js
@@ -16,17 +16,27 @@ app.get('/' , (req ,res ) => {
         response.on('data', (data) => {
            const weatherData =  JSON.parse(data);
            const temp = weatherData.main.temp
-           const des = weatherData.weather[0].description
-           
+           const des = weatherData.weather[0].description;
+           const icon = weatherData.weather[0].icon;
+           const imageURL ='http://openweathermap.org/img/wn/' + icon +'@2x.png'
+           // my way
+        //    const Temperature = 'The temperature in mumbai is ';
+        //    const degree = 'degree Celcius';
+        //    const weatherDesc = 'The weather is currently'
+        //    res.send(`<h1>${Temperature} ${temp} ${degree}, ${weatherDesc} ${des}</h1>`)
+
+        res.write('<p>The weather is currently ' + des + "</p>");
+        res.write('<h1>The temperature in mumbai is' + temp + 'degree Celcius.</h1>')
+        res.write('<img src=' + imageURL +  '>');
+        res.send()
         //    const object = {
         //     name : "dheeraj",
         //     favouriteFood : 'Ramen'
         //    }
         //    console.log(JSON.stringify(object));
-           console.log(temp ,des)
+        //    console.log(temp ,des)
         })
     })
-    res.send('HELLO');
 })
 
 
