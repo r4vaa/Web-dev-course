bb2cbba356446102e60f18a89df2cf1292d1c3ca
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-27 10:33:49 +0530

    changes

--- angelaYu/express/calculator/calculator.js
+++ angelaYu/express/calculator/calculator.js
@@ -1,9 +1,32 @@
 const express = require('express')
-const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', (req, res) => {
+    const num1 = Number(req.body.n1);
+    const num2 = Number(req.body.n2);
+    const result = num1 + num2;
+    res.send('The result of the calculation is ' + result);
+})
+
+app.get('/bmi', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+})
+
+app.post('/bmicalculator', (req , res) => {
+    const weight =  parseFloat(req.body.weight);
+    const height =  parseFloat(req.body.height);
+    const BMI = weight / (height * height);
+    res.send('Your BMI is: ' + BMI);
 })
 
 
