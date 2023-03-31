d2e1e62359e9032619f3cc09011812fbb77cf839
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-03-31 08:34:38 +0530

    CRUD with mongoose

--- angelaYu/mongo/fruitsProject/app.js
+++ angelaYu/mongo/fruitsProject/app.js
@@ -6,7 +6,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB', { useNewUrlParser : true}
 // fruit schema
 const fruitSchema = new mongoose.Schema ({
     name : String,
-    rating :Number,
+        
+    rating : {
+        type : Number,
+        min : 1,
+        max: 10
+    },
     review : String
 })
 
@@ -18,61 +23,97 @@ const Fruit = mongoose.model("Fruit", fruitSchema);
 // create a new fruit
 
 
-const fruit = new Fruit ({
-    name : 'Apple',
-    rating : 7,
-    review : "pretty good/"
+const jambul = new Fruit ({
+    name : 'Jambul',
+    rating : 10  ,
+    review : "noiceeee"
 })
 
-//fruit.save()
+jambul.save()
 
-// create person schema
 const personSchema = new mongoose.Schema ({
     name : String,
-    age : Number
+    age : Number,
+    favouriteFruit : fruitSchema
 })
 
 //person model
 const Person = mongoose.model("Person", personSchema);
 
-
-// create new person
-const person = new Person ({
-    name : 'Dheeraj Patil',
-    age : 27
-})
-
-// person.save();
-
-const kiwi = new Fruit({
-    name : 'Kiwi',
-    score : 10,
-    review : 'The best fruit'
-})
-
-const orange = new Fruit({
-    name : 'Orange',
-    score : 11,
-    review : 'TTo good for me '
-})
- 
-const banana = new Fruit({
-    name : 'Banana',
-    score : 7,
-    review : 'Weird Texture'
-})
-
-const dalim = new Fruit({
-    name : 'Dalim',
-    score : 7,
-    review : 'nice juicy'
-})
-
-Fruit.insertMany([kiwi , orange ,banana])
+Person.updateOne({name: "Dheeraj Patil"},{favouriteFruit : jambul})
 .then(function(err) {
     if(err){
         console.log(err);
-    } else {
-        console.log('Successfully saved all the fruits in the fruitsDB')
+    }else{
+        console.log('Successfully updated');
     }
 })
+
+
+// Fruit.updateOne({_id: "64259d8ea585d3438ed3e865"} ,{ name : "Safed jambun"})
+// .then(res => console.log(res, 'Successfully updated the doc'));
+
+// Fruit.deleteOne({_id:"64259d8ea585d3438ed3e865"})
+// .then(res => console.log(res, "successfully Delete the FILE"))
+
+// create person schema
+
+
+
+
+
+// create new person
+// const person = new Person ({
+//     name : 'Priyanka Patil',
+//     age : 27,
+//     favouriteFruit : mango
+// })
+
+
+// person.save();
+
+// const kiwi = new Fruit({
+//     name : 'Kiwi',
+//     score : 10,
+//     review : 'The best fruit'
+// })
+
+// const orange = new Fruit({
+//     name : 'Orange',
+//     score : 11,
+//     review : 'TTo good for me '
+// })
+ 
+// const banana = new Fruit({
+//     name : 'Banana',
+//     score : 7,
+//     review : 'Weird Texture'
+// })
+
+// const dalim = new Fruit({
+//     name : 'Dalim',
+//     score : 7,
+//     review : 'nice juicy'
+// })
+
+// Fruit.insertMany([kiwi , orange ,banana])
+// .then(function(err) {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log('Successfully saved all the fruits in the fruitsDB')
+//     }
+// })
+
+// Fruit.find({}).then(data => {
+//     data.forEach(d => {
+//         console.log(d.name);
+    
+//     })
+// }
+// )
+
+// Fruit.find()
+// .then(function(fruits){fruits.forEach(function(fruit){
+//     console.log(fruit.name)
+// })});
