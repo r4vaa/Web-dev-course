ea4f35873267453458c83b5c499b36d68bd2b3be
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-11 17:39:41 +0530

    react

---
+++ angelaYu/react/my-app/my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <h1>hello WOrld!</h1>
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<h1>Hello Worlds!!</h1>, document.getElementById('root'));
+
